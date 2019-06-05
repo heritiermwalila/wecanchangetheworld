820fe2b82791e36475b78c9e82f6a391488bbc0d
dev-admin <admin@admins-MacBook-Pro.local>
2019-06-05 14:13:19 +0200

    align settings files

--- wecanchange/settings/stage.py
+++ wecanchange/settings/stage.py
@@ -21,12 +21,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__fil
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', 'wecanw3(747jlc%(1gm331ky3&m3j@vrjct2f-6u0rlv2^=%wk!!wo=change')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['stage-wecanchange.herokuapp.com']
 
 
 # Application definition
